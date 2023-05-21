c421989171998079783b03ef0c4654c36d295828
Sophie Herold <sophie@hemio.de>
2023-05-21 22:12:20 +0200

    support image-rs complete

--- glycin-crates/decoders/glycin-image-rs/src/bin/glycin-image-rs.rs
+++ glycin-crates/decoders/glycin-image-rs/src/bin/glycin-image-rs.rs
@@ -120,37 +120,93 @@ impl Decoder for ImgDecoder {
     }
 }
 
-pub enum ImageRsDecoder<T: std::io::Read> {
-    Jpeg(codecs::jpeg::JpegDecoder<T>),
-    Png(codecs::png::PngDecoder<T>),
+pub enum ImageRsDecoder<T: std::io::Read + std::io::Seek> {
+    Bmp(codecs::bmp::BmpDecoder<T>),
+    Dds(codecs::dds::DdsDecoder<T>),
+    Farbfeld(codecs::farbfeld::FarbfeldDecoder<T>),
     Gif(codecs::gif::GifDecoder<T>),
+    //Hdr(codecs::hdr::HdrDecoder<T>),
+    Ico(codecs::ico::IcoDecoder<T>),
+    Jpeg(codecs::jpeg::JpegDecoder<T>),
+    OpenExr(codecs::openexr::OpenExrDecoder<T>),
+    Png(codecs::png::PngDecoder<T>),
+    Pnm(codecs::pnm::PnmDecoder<T>),
+    Qoi(codecs::qoi::QoiDecoder<T>),
+    Tga(codecs::tga::TgaDecoder<T>),
+    Tiff(codecs::tiff::TiffDecoder<T>),
+    WebP(codecs::webp::WebPDecoder<T>),
 }
 
 impl ImageRsDecoder<Reader> {
     fn new(data: Reader, mime_type: &str) -> Result<Self, DecoderError> {
         Ok(match mime_type {
-            "image/jpeg" => Self::Jpeg(codecs::jpeg::JpegDecoder::new(data).context_failed()?),
-            "image/png" => Self::Png(codecs::png::PngDecoder::new(data).context_failed()?),
+            "image/bmp" => Self::Bmp(codecs::bmp::BmpDecoder::new(data).context_failed()?),
+            "image/vnd-ms.dds" => Self::Dds(codecs::dds::DdsDecoder::new(data).context_failed()?),
+            "image/x-ff" => {
+                Self::Farbfeld(codecs::farbfeld::FarbfeldDecoder::new(data).context_failed()?)
+            }
             "image/gif" => Self::Gif(codecs::gif::GifDecoder::new(data).context_failed()?),
+            //"image/vnd.radiance" => Self::Hdr(codecs::hdr::HdrDecoder::new(data).context_failed()?),
+            "image/x-icon" => Self::Ico(codecs::ico::IcoDecoder::new(data).context_failed()?),
+            "image/jpeg" => Self::Jpeg(codecs::jpeg::JpegDecoder::new(data).context_failed()?),
+            "image/x-exr" => {
+                Self::OpenExr(codecs::openexr::OpenExrDecoder::new(data).context_failed()?)
+            }
+            "image/png" => Self::Png(codecs::png::PngDecoder::new(data).context_failed()?),
+            "image/x-portable-bitmap"
+            | "image/x-portable-graymap"
+            | "image/x-portable-pixmap"
+            | "image/x-portable-anymap" => {
+                Self::Pnm(codecs::pnm::PnmDecoder::new(data).context_failed()?)
+            }
+            "image/x-qoi" => Self::Qoi(codecs::qoi::QoiDecoder::new(data).context_failed()?),
+            "image/x-targa" | "image/x-tga" => {
+                Self::Tga(codecs::tga::TgaDecoder::new(data).context_failed()?)
+            }
+            "image/tiff" => Self::Tiff(codecs::tiff::TiffDecoder::new(data).context_failed()?),
+            "image/webp" => Self::WebP(codecs::webp::WebPDecoder::new(data).context_failed()?),
+
             _ => return Err(DecoderError::UnsupportedImageFormat),
         })
     }
 }
 
-impl<'a, T: std::io::Read + 'a> ImageRsDecoder<T> {
+impl<'a, T: std::io::Read + std::io::Seek + 'a> ImageRsDecoder<T> {
     fn info(&mut self) -> ImageInfo {
         match self {
-            Self::Jpeg(d) => ImageInfo::from_decoder(d, "JPEG"),
-            Self::Png(d) => ImageInfo::from_decoder(d, "PNG"),
+            Self::Bmp(d) => ImageInfo::from_decoder(d, "BMP"),
+            Self::Dds(d) => ImageInfo::from_decoder(d, "DDS"),
+            Self::Farbfeld(d) => ImageInfo::from_decoder(d, "Farbfeld"),
             Self::Gif(d) => ImageInfo::from_decoder(d, "GIF"),
+            //Self::Hdr(d) => ImageInfo::from_decoder(d, "Radiance HDR"),
+            Self::Ico(d) => ImageInfo::from_decoder(d, "ICO"),
+            Self::Jpeg(d) => ImageInfo::from_decoder(d, "JPEG"),
+            Self::OpenExr(d) => ImageInfo::from_decoder(d, "OpenEXR"),
+            Self::Png(d) => ImageInfo::from_decoder(d, "PNG"),
+            Self::Pnm(d) => ImageInfo::from_decoder(d, "PNM"),
+            Self::Qoi(d) => ImageInfo::from_decoder(d, "QOI"),
+            Self::Tga(d) => ImageInfo::from_decoder(d, "TGA"),
+            Self::Tiff(d) => ImageInfo::from_decoder(d, "TIFF"),
+            Self::WebP(d) => ImageInfo::from_decoder(d, "WebP"),
         }
     }
 
     fn frame(self) -> Result<Frame, image::ImageError> {
         match self {
-            Self::Jpeg(d) => Frame::from_decoder(d),
-            Self::Png(d) => Frame::from_decoder(d),
+            Self::Bmp(d) => Frame::from_decoder(d),
+            Self::Dds(d) => Frame::from_decoder(d),
+            Self::Farbfeld(d) => Frame::from_decoder(d),
             Self::Gif(d) => Frame::from_decoder(d),
+            //Self::Hdr(d) => Frame::from_decoder(d),
+            Self::Ico(d) => Frame::from_decoder(d),
+            Self::Jpeg(d) => Frame::from_decoder(d),
+            Self::OpenExr(d) => Frame::from_decoder(d),
+            Self::Png(d) => Frame::from_decoder(d),
+            Self::Pnm(d) => Frame::from_decoder(d),
+            Self::Qoi(d) => Frame::from_decoder(d),
+            Self::Tga(d) => Frame::from_decoder(d),
+            Self::Tiff(d) => Frame::from_decoder(d),
+            Self::WebP(d) => Frame::from_decoder(d),
         }
     }
 
@@ -164,8 +220,9 @@ impl<'a, T: std::io::Read + 'a> ImageRsDecoder<T> {
 
     fn is_animated(&self) -> bool {
         match self {
-            Self::Png(d) => d.is_apng(),
             Self::Gif(_) => true,
+            Self::Png(d) => d.is_apng(),
+            Self::WebP(d) => d.has_animation(),
             _ => false,
         }
     }
