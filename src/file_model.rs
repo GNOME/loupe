229c63719ec4ce0e1174f4949b4387dee924d1c1
Sophie Herold <sophie@hemio.de>
2023-11-21 20:55:03 +0100

    meta: Prepare 45.2

--- src/file_model.rs
+++ src/file_model.rs
@@ -1,5 +1,6 @@
 // Copyright (c) 2022-2023 Sophie Herold
 // Copyright (c) 2022-2023 Christopher Davis
+// Copyright (c) 2023 Julian Hofer
 // Copyright (c) 2023 Gage Berz
 //
 // This program is free software: you can redistribute it and/or modify
--- src/widgets/sliding_view.rs
+++ src/widgets/sliding_view.rs
@@ -1,5 +1,6 @@
-// Copyright (c) 2023 Christopher Davis
 // Copyright (c) 2023 Sophie Herold
+// Copyright (c) 2023 Alice Mikhaylenko
+// Copyright (c) 2023 Christopher Davis
 // Copyright (c) 2023 Lubosz Sarnecki
 //
 // This program is free software: you can redistribute it and/or modify
