6cd7efce43e26ec6251bfa5fcf17ddaa519d5554
Sophie Herold <sophie@hemio.de>
2025-12-09 15:55:30 +0100

    util/root: Use specific function to show toasts

--- src/widgets/image_window.rs
+++ src/widgets/image_window.rs
@@ -597,7 +597,7 @@ impl LpImageWindow {
         if let Err(e) = imp.image_view.copy() {
             log::error!("Failed to copy to clipboard: {}", e);
         } else {
-            self.window().show_toast(
+            self.window_show_toast(
                 &gettext("Image copied to clipboard"),
                 adw::ToastPriority::High,
             );
@@ -634,7 +634,7 @@ impl LpImageWindow {
                             let result = crate::util::untrash(&path).await;
                             if let Err(err) = result {
                                 log::error!("Failed to untrash {path:?}: {err}");
-                                obj.window().show_toast(
+                                obj.window_show_toast(
                                     &gettext("Failed to restore image from trash"),
                                     adw::ToastPriority::High,
                                 );
@@ -642,14 +642,14 @@ impl LpImageWindow {
                         });
                     }
                 ));
-                self.window().add_toast(toast);
+                self.window_add_toast(toast);
             }
             Err(err) => {
                 if Some(gio::IOErrorEnum::NotSupported) == err.kind::<gio::IOErrorEnum>() {
                     self.delete_future(&path).await;
                 } else {
                     log::error!("Failed to delete file {path:?}: {err}");
-                    self.window().show_toast(
+                    self.window_show_toast(
                         &gettext("Failed to move image to trash"),
                         adw::ToastPriority::Normal,
                     );
@@ -695,7 +695,7 @@ impl LpImageWindow {
 
             if let Err(err) = result {
                 log::error!("Failed to delete file {path:?}: {err}");
-                self.window().show_toast(
+                self.window_show_toast(
                     &gettext("Failed to delete image"),
                     adw::ToastPriority::Normal,
                 );
